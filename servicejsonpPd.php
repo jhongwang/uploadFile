5189d5a0ce07d0a49048aae7a6477d64cae8f410
v_jhongwang(王佳虹) <v_jhongwang@tencent.com>
2017-04-07 19:32:58 +0800

    twophp

--- servicejsonpPd.php
+++ servicejsonpPd.php
@@ -2,24 +2,24 @@
 //页面中./upload是地址路径
 $file = $_FILES['mof'];
 if($file['error']==0){//不存在时候
-echo 61;
+echo 71;
  if(!file_exists('./upload/'.$_POST['test'])){
- 	echo 611;
+ 	echo 711;
   if(move_uploaded_file($file['tmp_name'],'./upload/'.$_POST['test'])){
-  	echo 6111;
+  	echo 7111;
    echo 'fileFailed1';
   }
  }else{//同名的走这里
- 	echo 612;
+ 	echo 712;
   //$content=file_get_contents($file['tmp_name']);
   unlink('./upload/'.$_POST['test']);
   /*if (!file_put_contents('./upload/'.$_POST['test'], $content,FILE_APPEND)) {
-  	echo 6121;
+  	echo 7121;
    echo 'fileFailed2';
   }*/
  }
 }else{
- echo 62;
+ echo 72;
  echo 'fileFailed3';
 }
  
